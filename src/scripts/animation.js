b0afe701dd800430ac802adc4dd75da35ec18b0a
kw-8 <kwu11235813@gmail.com>
2021-11-03 16:43:08 -0700

    gooey effect added, scss, html, and animation files updated

--- src/scripts/animation.js
+++ src/scripts/animation.js
@@ -1,29 +1,18 @@
 async function splashRelated() {
   // greensock to calculate position transformations
   // html filters
-  // gsap.to('.related-article-list li', { duration: 3, rotation: 360, });
-  let screen = document.querySelector('html');
+  let screen = document.querySelector('.fade-bg');
   
   let articleList = Array.from(document.querySelectorAll('.related-article-list li'));
-  // console.log(articleList, 'help');
-  // let angle = (2*0 + 1) * (2 * Math.PI / articleList.length); // -PI to PI = -1 to 1
-  // let radius = Math.min(screen.scrollWidth, screen.scrollHeight)/2 - 36;
-  // let transformDir = [radius * Math.cos(angle), radius * Math.sin(angle)];
-  // console.log(angle, radius, transformDir);
-
-  // // gsap.to(`.related-article-list li:nth-child(${0})`, { duration: 3, rotation: 360 });
-  // console.log(document.querySelector(`.related-article-list li:nth-child(${1})`));
-  
-  // console.log(document.querySelector('.related-article-list li'));
   
   articleList.forEach((child, i) =>{
     let n = 2 * i + 1;
     let angle = n * (Math.PI/articleList.length); // -PI to PI = -1 to 1
-    let radius = Math.min(screen.scrollWidth, screen.scrollHeight)/2 - 36;
+    let radius = 0.3 * Math.min(screen.scrollWidth, screen.scrollHeight);
     let transformDir = [radius * Math.cos(angle), radius * Math.sin(angle)];
-    console.log(transformDir);
+    // gsap.to(`.related-article-list li:nth-child(${n})`, { scale: 3 });
     gsap.to(`.related-article-list li:nth-child(${n})`,
-            {duration: 3, x: transformDir[0], y: transformDir[1]});
+      { duration: 1, x: transformDir[0], y: transformDir[1], scale: 1.5});
   });
 }
 
